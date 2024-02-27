76152021727afbc6725c3da8974410129b3cfb7e
Nanditha M <nanditham.bsc23@rvu.edu.in>
2024-02-27 14:20:35 +0530

    test

---
+++ set04/problem03.c
@@ -0,0 +1,10 @@
+// Write a program to find the `nCr` of given n and r
+
+#include<stdio.h>
+void input_n_and_r(int *n, int *r) {
+    printf("Enter the value of n: ");
+    scanf("%d", n);
+    printf("Enter the value of r: ");
+    scanf("%d", r);
+}
+
