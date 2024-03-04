9711cf17b3d84814b56e47ccc53d188d89681fc5
Nanditha M <nanditham.bsc23@rvu.edu.in>
2024-03-04 14:00:01 +0530

    test

--- set04/problem03.c
+++ set04/problem03.c
@@ -8,3 +8,21 @@ void input_n_and_r(int *n, int *r) {
     scanf("%d", r);
 }
 
+int nCr(int n, int r) {
+    if (r == 0 || r == n) {
+        return 1;
+    } else {
+        return nCr(n - 1, r - 1) + nCr(n - 1, r);
+    }
+}
+
+void output(int n, int r, int result) {
+    printf("The value of %dC%d is: %d\n", n, r, result);
+}
+int main() {
+    int n, r;
+    input_n_and_r(&n, &r); 
+    int result = nCr(n, r);
+    output(n, r, result); 
+    return 0;
+}
